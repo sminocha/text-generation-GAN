964f91fc5db28d5761f7c533fdf097fea250191e
Ryan <ryangmui@gmail.com>
2018-05-30 16:33:30 -0700

    parser

---
+++ parser.py
@@ -0,0 +1,19 @@
+from nltk.tree import Tree
+#import pandas as pd
+import sst
+
+data =[]
+for y in sst.train_reader():
+	sent = ""
+	for tree in y:
+		if type(tree) == Tree:
+			sent = " ".join(tree.leaves())
+		else:
+			sent += " " + str(tree)
+	data.append(sent)
+
+print(len(data))
+f = open("processed_train_data.txt", 'w+')
+for sent in data:
+	f.write(sent + "\n")
+#train_labels = [y for tree, y in sst.train_reader()]
